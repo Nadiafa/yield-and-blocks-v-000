6f26b8d29fff164837b6916db91a14b9977b4b2a
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-16 10:17:27 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,18 +1,5 @@
 name=["Tim", "Tom", "Jim"]
 
-def hi_t(array)
-  i=0 
-  while i<array.length && array[i].start_with?("T")
-    puts "Hi #{array[i]}"
-    i+=1
-  end
-
-end
-
-hi_t(name)
-
-puts "-----------------------------------------------"
-
 def hello_t(array)
   i=0 
   while i<array.length 
